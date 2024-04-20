0ab0d856fc87fc0085ab03cc1b529bd1bc7fd084
AdrianR0driguez <007923676@coyote.csusb.edu>
2024-04-19 22:14:01 -0700

    Finished iterative search trying to figure out binary search

---
+++ search.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int iterativeSearch(vector<int>v, int elem){
+    int i;
+    for(i = 0; i = SIZE; i++){
+        if(v[i] == elem){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int binarySearch(vector<int> & v, int start, int end, int elem){
+    int mid;
+    if(elem = mid){
+        return -1;
+    }else{
+        
+    }
+}
